ff5d3d6c76032da9e163c002163b2c6459550de1
abdulrazack-dumale <musharfabdul@gmail.com>
2022-08-23 20:53:33 +0300

    finished assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newForm = document.querySelector("#create-task-form"); // getting the <form> by Id
+  newForm.addEventListener("submit", createNewTask); // adding an event listener to the form
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newLable = document.querySelector("#new-task-description"); //getting the <label> by Id
+  const newTask = document.createElement("li");  // created a <li> tag is inside the <ul> as a child which is inside the <div>
+  newTask.innerText = newLable.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+const appendNewTask = task => {
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x ' // this button will be inside the <li> as a child
+  task.appendChild(btn)
+  console.log(task)
+  document.querySelector("#tasks").appendChild(task);
+};
+
+function handleDelete(e){ // so that we can remove the list 
+  e.target.parentNode.remove()
+}
+
